f21143848c0447c21554814f708c31daf3b4aa54
揽月 <2727867180@qq.com>
2026-05-16 16:50:20 +0800

    admin模块的分组功能-删除分组，修改了查询分组查到软删除的分组bug

--- admin/src/main/java/com/lanyue/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/lanyue/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,7 +1,7 @@
 package com.lanyue.shortlink.admin.service.impl;
 
 import cn.hutool.core.util.StrUtil;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -42,9 +42,11 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public List<GroupRespDTO> listGroup() {
-        QueryWrapper<GroupDO> queryWrapper = new QueryWrapper<GroupDO>()
-                .eq(CommonConstant.USERNAME, UserContext.getUsername());
-        List<GroupDO> groupDOS = baseMapper.selectList(queryWrapper);
+        LambdaQueryWrapper<GroupDO> wrapper = new LambdaQueryWrapper<>(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOS = baseMapper.selectList(wrapper);
         return groupDOS.stream().map(groupDO -> GroupRespDTO.builder()
                 .gid(groupDO.getGid())
                 .name(groupDO.getName())
@@ -114,6 +116,15 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void deleteGroup(GroupDeleteReqDTO requestParam) {
+        LambdaUpdateWrapper<GroupDO> updateWrapper = Wrappers.lambdaUpdate(GroupDO.class)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .eq(GroupDO::getGid, requestParam.getId())
+                .eq(GroupDO::getDelFlag, 0);
+        GroupDO groupDO = new GroupDO();
+        groupDO.setDelFlag(1);
+        baseMapper.update(groupDO, updateWrapper);
+        //TODO 分组下的短链接删除
+
 
     }
 }
